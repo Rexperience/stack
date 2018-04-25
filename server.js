39cdc9e05b9531bd1951e6fef94473a0104e0433
Rexperience <findmeanyway@gmail.com>
2018-04-25 21:03:04 +0700

    add landing page for the api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
@@ -8,6 +9,7 @@ const mysql = require('mysql2')
 const cors = require('cors')
 
 const app = express()
+const router = express.Router()
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -15,12 +17,15 @@ app.use(compression())
 app.use(helmet())
 app.use(cors())
 
+//make root dir serve landing page
+app.use('/', express.static('src'))
+
 const port = process.env.PORT || 3000
 
 // Configure database connect here
 
 app.get('/', (req, res) => {
-    res.send('Ohai!')
+    console.log('Ohai!');
 })
 
 app.listen(port, () => {
