ac33d3d8680fae20422f2d803845bda9259a6bb3
Rexperience <findmeanyway@gmail.com>
2018-04-25 22:25:40 +0700

    Test database connection and remove client folder(too much bloat)

--- server.js
+++ server.js
@@ -23,9 +23,42 @@ app.use('/', express.static('src'))
 const port = process.env.PORT || 3000
 
 // Configure database connect here
+var database = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database:'blog'
+})
+
+database.connect()
 
 app.get('/', (req, res) => {
     console.log('Ohai!');
+    concole.log(Date.now());
+})
+
+app.post('/create', (req, res) => {
+    let q = 'insert into post (_title, _post) values ("This is title", "Lorem ipsum dolot sit amet");'
+    database.query(q, (err, result) => {
+        if (err) throw err
+        console.debug('Post Created!')
+    })
+})
+
+// app.get('/show', (req, res) => {
+//     let q = 'show tables;'
+//     database.query(q, (err, result) => {
+//         if (err) throw err
+//         console.debug('Tables accesed with no error.')
+//         res.send(JSON.stringify(result))
+//     })
+// })
+
+app.get('/post', (req, rest) => {
+    let q = 'select * from post;'
+    database.query(q, (err, result) => {
+        if(err) throw err
+        console.log('Post fetched')
+    })
 })
 
 app.listen(port, () => {
